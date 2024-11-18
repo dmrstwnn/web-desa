d48cd4e30ced4b835b03fe374d8873b1b4daa6a3
dmrstwnn <pkldamar@gmail.com>
2024-11-19 00:00:37 +0700

    ui

--- resources/views/index.blade.php
+++ resources/views/index.blade.php
@@ -44,9 +44,22 @@
                                 <div class="md:h-16 h-16 mx-auto md:px-4 container flex items-center justify-between flex-wrap md:flex-nowrap">
                                     <div class="text-gray-800 order-3 w-full md:w-auto md:order-2">
                                         <ul class="flex font-semibold justify-between">
-                                            <li class="md:px-4 md:py-2 text-gray-800 hover:text-gray-900 transition duration-300 ease-in-out font-semibold">
-                                                <a href="/index">Beranda</a>
+                                            <li class="relative font-semibold   " x-data="{ open: false }">
+                                                <button @click="open = !open" class="md:px-4 md:py-2 text-gray-800 hover:text-gray-900 flex items-center">
+                                                    Beranda
+                                                    <svg class="w-4 h-4 ml-2 -mr-1" xmlns="http://www.w3.org/2000/svg" viewBox="0 0 20 20" fill="currentColor">
+                                                        <path fill-rule="evenodd" d="M10 12l-5-5h10l-5 5z" />
+                                                    </svg>
+                                                </button>
+                                                <div x-show="open" @click.away="open = false" class="absolute left-0 w-40 mt-1 bg-white rounded-md shadow-lg">
+                                                    <a href="#berita" class="block px-4 py-2 text-sm text-gray-700 hover:text-indigo-400">Berita</a>
+                                                    <a href="#profil" class="block px-4 py-2 text-sm text-gray-700 hover:text-indigo-400">Profil</a>
+                                                    <a href="#lokasi" class="block px-4 py-2 text-sm text-gray-700 hover:text-indigo-400">Lokasi</a>
+                                                </div>
                                             </li>
+                                            {{-- <li class="md:px-4 md:py-2 text-gray-800 hover:text-gray-900 transition duration-300 ease-in-out font-semibold">
+                                                <a href="/">Beranda</a>
+                                            </li> --}}
                                             <li class="md:px-4 md:py-2 text-gray-800 hover:text-gray-900 transition duration-300 ease-in-out font-semibold">
                                                 <a href="/profil">Profil</a>
                                             </li>
@@ -56,7 +69,8 @@
                                             <li class="md:px-4 md:py-2 text-gray-800 hover:text-gray-900 transition duration-300 ease-in-out font-semibold">
                                                 <a href="#">Peraturan Desa</a>
                                             </li>
-                                            <li class="relative" x-data="{ open: false }">
+                                            
+                                            <li class="relative font-semibold" x-data="{ open: false }">
                                                 <button @click="open = !open" class="md:px-4 md:py-2 text-gray-800 hover:text-gray-900 flex items-center">
                                                     Menu
                                                     <svg class="w-4 h-4 ml-2 -mr-1" xmlns="http://www.w3.org/2000/svg" viewBox="0 0 20 20" fill="currentColor">
@@ -83,11 +97,10 @@
     {{-- <section id="home" class="min-h-screen flex items-center justify-center bg-center bg-no-repeat">
          --}}
          <section id="home" class="min-h-screen flex items-center justify-center bg-center bg-no-repeat relative bg-cover" style="background-image: url('/image/kalibenda.jpg');">
-
             <!-- Teks di tengah -->
             <div class="relative text-center">
-                <h2 class="text-6xl text-effect md:text-5xl font-bold text-white mb-4 text-shadow-lg">Selamat Datang di Desa Kalibenda</h2>
-                <p class="text-lg text-white mb-16 text-shadow-md">Desa yang asri dengan berbagai potensi alam dan budaya.</p>
+                <h2 class="bg-blue-500 rounded-lg px-3 pb-2 bg-bottom text-6xl text-effect md:text-5xl font-bold text-white mb-4 text-shadow-lg">Selamat Datang di Desa Kalibenda</h2>
+                <p class=" text-lg text-white mb-16 text-shadow-md"><span class="bg-blue-500 rounded-md px-2 bg-bottom">Desa yang asri dengan berbagai potensi alam dan budaya.</span></p>
                  <a href="#about" 
                  class="inline-block text-white bg-blue-600 border border-transparent rounded-full text-xl font-semibold py-4 px-12 
                         transition-all duration-300 ease-in-out transform 
@@ -105,13 +118,12 @@
         
     
             <!-- Berita Section -->
-            <section id="berita" class="pb-20 pt-16 bg-white">
+            <section id="berita" class="py-20 bg-white">
                 <div class="max-w-6xl mx-auto px-4 sm:px-6 lg:px-8">
                     <div class="text-left mb-5">
-                        <h2 class="text-4xl font-bold hover-underline-berita text-gray-900 relative inline-block">
-                            {{-- <span class="absolute inset-x-0 bottom-0 h-1 bg-blue-600"></span> --}}
+                        <a href="/berita" id="hover-berita" class="text-4xl font-bold hover-underline-berita text-gray-900 relative inline-block">
                             Berita Terbaru
-                        </h2>
+                        </a>
                         <p class="mt-3 text-gray-600">Dapatkan informasi terbaru seputar kegiatan dan peristiwa di Desa Kalibenda.</p>
                     </div>
 
@@ -147,32 +159,74 @@
                         
                     </div>
                 </div>
+                <div class="text-center mt-14">
+                    <a href="/berita" 
+                       class="inline-flex text-center justify-center items-center px-7 py-4 text-base font-medium text-white bg-blue-600 rounded-lg shadow hover:bg-blue-700 focus:ring-4 focus:ring-blue-300 focus:outline-none dark:bg-blue-500 dark:hover:bg-blue-600 dark:focus:ring-blue-800">
+                        Lihat Semua Berita
+                        <svg class="w-4 h-4 ml-2" xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" stroke-width="2" stroke="currentColor" aria-hidden="true">
+                            <path stroke-linecap="round" stroke-linejoin="round" d="M5 12h14m0 0l-6 6m6-6l-6-6"/>
+                        </svg>
+                    </a>
+                </div>
             </section>
 
             <!-- Profil Section -->
-            <section id="about" class="py-20 bg-gray-100">
+            <section id="profil" class="py-20 bg-gray-100">
                 <div class="max-w-6xl mx-auto px-4 sm:px-6 lg:px-8">
                     <div class="text-center mb-10">
-                        <h2 class="text-5xl font-bold text-gray-900">Profil Desa Kalibenda</h2>
+                        <h2 class="text-4xl font-bold text-gray-900">Profil Desa Kalibenda</h2>
                         <p class="mt-4 text-black text-lg font-semibold">Sekilas tentang sejarah, visi, dan misi Desa Kalibenda.</p>
                     </div>
                     <div class="grid grid-cols-1 md:grid-cols-2 gap-x-4">
                         <img src="/image/kalibenda.jpg" alt="Profil Desa" class="h-auto max-w-lg rounded-lg">
                         <div class="flex flex-col">
                             <p class="mb-4 text-gray-900 leading-relaxed font-medium">Desa Kalibenda terletak di Kecamatan Ajibarang, Kabupaten Banyumas, Jawa Tengah, dan memiliki sejarah yang panjang serta budaya yang kental. Desa ini merupakan salah satu desa tua yang telah ada sejak masa kolonial Belanda, dan kini terus berkembang sebagai desa yang mempertahankan nilai-nilai tradisional.</p>
-                            <p class="text-gray-900 leading-relaxed font-medium">Pada masa penjajahan Belanda, Desa Kalibenda menjadi salah satu pusat perlawanan rakyat Banyumas. Penduduk desa yang mayoritas petani ikut berjuang melawan penjajah, menjadikan desa ini penting dalam sejarah perjuangan rakyat di wilayah Banyumas. Semangat kebersamaan dan gotong royong yang diwariskan dari masa itu masih menjadi ciri khas kehidupan sosial masyarakat Kalibenda hingga kini.....</p>
+                            <p class="text-gray-900 leading-relaxed font-medium">Pada masa penjajahan Belanda, Desa Kalibenda menjadi salah satu pusat perlawanan rakyat Banyumas......</p>
                             <a href="/profil" class="text-blue-600 mt-4 inline-flex items-center hover-underline relative text-base font-normal hover:text-blue-800 origin-center">Pelajari lebih lanjut &nbsp; &raquo;</a>
                         </div>
                     </div>                    
                 </div>
             </section>
+            
+            <section id="lokasi" class="bg-white pt-5">
+                <div class="max-w-6xl mx-auto px-4 sm:px-6 lg:py-16 lg:px-8">
+                    <div class="max-w-xl lg:max-w-4xl mx-auto text-center ">
+                        <h2 class="text-4xl font-bold text-gray-900">Lokasi Desa <span class="">Kali Benda</span></h2>
+                        <p class="mt-1 text-sm text-gray-500">Lorem ipsum dolor sit amet consectetur adipisicing elit.</p>
+                    </div>
+                    <div class="mt-8 lg:mt-12">
+                        <div class="grid grid-cols-1 md:grid-cols-2 gap-5">
+                            <div class="rounded-lg overflow-hidden">
+                                <iframe 
+                                src="https://www.google.com/maps/embed?pb=!1m14!1m8!1m3!1d15825.874726253829!2d109.096026!3d-7.4132665!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x2e6561645c41abb7%3A0xb8d0961320615f12!2sKalibenda%2C%20Kec.%20Ajibarang%2C%20Kabupaten%20Banyumas%2C%20Jawa%20Tengah!5e0!3m2!1sid!2sid!4v1731939319425!5m2!1sid!2sid"
+                                width="100%" height="330" style="border:0;" allowfullscreen="" loading="lazy" referrerpolicy="no-referrer-when-downgrade"></iframe>
+                            </div>
+                            <div>
+                                <div class="max-w-full mx-auto rounded-lg overflow-hidden">
+                                    <div class="px-6 py-4">
+                                        <h3 class="text-lg font-medium text-gray-900">Alamat Kami</h3>
+                                        <p class="mt-1 text-gray-600">Kalibenda, 
+                                            Kec. Ajibarang,
+                                            Kabupaten Banyumas,<br>
+                                            Jawa Tengah,	
+                                            53163.</p>
+                                    </div>
+                                </div>
+                            </div>
+                        </div>
+                    </div>
+                </div>
+            </section>
 
             <!-- Footer Section -->
-            <footer class="bg-gray-800 text-gray-300 py-6">
-                <div class="max-w-7xl mx-auto px-4  sm:px-6 lg:px-8 text-center">
+            <footer class="bg-gray-800 text-gray-300 py-12">
+                <div class="max-w-sm px-4 ml-10 sm:px-6 lg:px-8 text-left">
+                    <p>Email: info@example.com</p>
+                    <p class="border-b pb-1 border-gray-200">Telepon: +62 2349 4349 6593</p>
                     <p>&copy; 2024 Desa Kalibenda.</p>
                 </div>
             </footer>
+            
 
 </nav>     
 </div>
@@ -180,7 +234,7 @@
 
 
 
-<script>
+<script> //navbar
     const navbar = document.querySelector('nav');
     
     window.addEventListener('scroll', () => {
@@ -197,7 +251,7 @@
       }
     });
 </script>
-<script>
+<script>    //card 
     document.querySelectorAll('.news-card').forEach((card) => {
         const observer = new IntersectionObserver((entries) => {
             entries.forEach((entry) => {
@@ -216,7 +270,29 @@
     }
 </script>
 <script>
-    
+document.addEventListener("DOMContentLoaded", function () {
+    const h2Berita = document.querySelector("#hover-berita");
+
+    // Buat Intersection Observer
+    const observer = new IntersectionObserver(
+        (entries, observer) => {
+            entries.forEach(entry => {
+                if (entry.isIntersecting) {
+                    // Tambahkan class 'animate' ke elemen
+                    h2Berita.classList.add("animate");
+
+                    // Hentikan observasi setelah animasi berjalan
+                    observer.unobserve(entry.target);
+                }
+            });
+        },
+        { threshold: 1 } // 50% elemen terlihat
+    );
+
+    // Pantau elemen <h2> "Berita Terbaru"
+    observer.observe(h2Berita);
+});
+
 </script>
 
 
--- resources/views/profil.blade.php
+++ resources/views/profil.blade.php
@@ -11,71 +11,112 @@
         <link rel="preconnect" href="https://fonts.bunny.net">
         <link href="https://fonts.bunny.net/css?family=figtree:400,500,600&display=swap" rel="stylesheet" />
         <link href="https://cdn.jsdelivr.net/npm/tailwindcss@2.2.19/dist/tailwind.min.css" rel="stylesheet">
+        <link rel="preconnect" href="https://fonts.googleapis.com">
+        <link rel="preconnect" href="https://fonts.gstatic.com" crossorigin>
+        <link href="https://fonts.googleapis.com/css2?family=Poppins:ital,wght@0,100;0,200;0,300;0,400;0,500;0,600;0,700;0,800;0,900;1,100;1,200;1,300;1,400;1,500;1,600;1,700;1,800;1,900&display=swap" rel="stylesheet">
         <script src="https://cdn.jsdelivr.net/npm/flowbite@1.3.3/dist/flowbite.min.js"></script>
+        <script src="https://cdn.jsdelivr.net/npm/alpinejs@3.x.x/dist/cdn.min.js" defer></script>
+
 
         <!-- Scripts -->
         @vite(['resources/css/app.css', 'resources/js/app.js'])
     </head>
-    <body class="font-sans antialiased">
+    <body class=" antialiased">
         <div class="min-h-screen bg-white">
             
-        <nav x-data="{ open: false }" class="bg-transparent z-10 border-b border-gray-100 shadow-md">
-    <!-- Primary Navigation Menu -->
-            <div class="w-full mx-auto px-4 sm:px-6 lg:px-8 fixed bg-white bg-transparent z-10  shadow-md">
-                <div class="flex justify-between items-center h-16">
-                    <div class="flex">
-                        <!-- Logo -->
-                        <div class="shrink-0 flex items-center">
-                            <a href="{{ route('dashboard') }}" class="ml-32">
-                            <img src="/image/bms.png" alt="" class="h-12 w-auto">
-                            </a>
-                        </div>
-
-                                <!-- component -->
-                        <nav class="w-100 px-36 md:px-auto justify-items-center">
-                            <div class="md:h-16 h-28 mx-auto md:px-4 container flex items-center justify-between flex-wrap md:flex-nowrap">
-                                <div class="text-gray-500 order-3 w-full md:w-auto md:order-2">
-                                    <ul class="flex font-semibold justify-between">
-                                        <!-- Active Link = text-indigo-500
-                                        Inactive Link = hover:text-indigo-500 -->
-                                        <li class="md:px-4 md:py-2 text-black hover:text-indigo-500 transition duration-300 ease-in-out font-semibold"><a href="/index">Beranda</a></li>
-                                        <li class="md:px-4 md:py-2 text-black hover:text-indigo-500 transition duration-300 ease-in-out font-semibold"><a href="/profil">Profil</a></li>
-                                        <li class="md:px-4 md:py-2 text-black hover:text-indigo-500 transition duration-300 ease-in-out font-semibold"><a href="#">Lembaga Desa</a></li>
-                                        <li class="md:px-4 md:py-2 text-black hover:text-indigo-500 transition duration-300 ease-in-out font-semibold"><a href="#">Peraturan Desa</a></li>
-                                        <li class="md:px-4 md:py-2 text-black hover:text-indigo-500 transition duration-300 ease-in-out font-semibold"><a href="#">Potensi</a></li>
-                                        <li class="md:px-4 md:py-2 text-black hover:text-indigo-500 transition duration-300 ease-in-out font-semibold"><a href="#">Menu Publik</a></li>
-                                        <li class="md:px-4 md:py-2 text-black hover:text-indigo-500 transition duration-300 ease-in-out font-semibold"><a href="#">Pelayanan</a></li>
-                                    </ul>
-                                </div>
+            <nav id="navbar" class="fixed top-0 w-full bg-white transition-all duration-300 shadow-xs z-50">
+                <div class="w-full mx-auto items-center shadow-md">
+                    <div class="h-16">
+                        <div class="flex justify-between items-center">
+                            <!-- Logo -->
+                            <div class="shrink-0 flex">
+                                <a href="{{ route('dashboard') }}" class="ml-16 flex gap-8 items-center">
+                                    <img src="/image/bms.png" alt="" class="h-12 w-auto">
+                                    <div class="text-sm text-gray-800">
+                                        <p>Desa Kalibenda Kec Ajibarang</p>
+                                        <p class="font-bold">Kab Banyumas</p>
+                                    </div>
+                                </a>
                             </div>
-                        </nav>
+            
+                            <!-- Navigation Links -->
+                            <nav class="w-100 pr-10 md:px-auto">
+                                <div class="md:h-16 h-16 mx-auto md:px-4 container flex items-center justify-between flex-wrap md:flex-nowrap">
+                                    <div class="text-gray-800 order-3 w-full md:w-auto md:order-2">
+                                        <ul class="flex font-semibold justify-between">
+                                            <li class="md:px-4 md:py-2 text-gray-800 hover:text-gray-900 transition duration-300 ease-in-out font-semibold">
+                                                <a href="/">Beranda</a>
+                                            </li>
+                                            <li class="md:px-4 md:py-2 text-gray-800 hover:text-gray-900 transition duration-300 ease-in-out font-semibold">
+                                                <a href="/profil">Profil</a>
+                                            </li>
+                                            <li class="md:px-4 md:py-2 text-gray-800 hover:text-gray-900 transition duration-300 ease-in-out font-semibold">
+                                                <a href="#">Lembaga Desa</a>
+                                            </li>
+                                            <li class="md:px-4 md:py-2 text-gray-800 hover:text-gray-900 transition duration-300 ease-in-out font-semibold">
+                                                <a href="#">Peraturan Desa</a>
+                                            </li>
+                                            
+                                            <li class="relative font-semibold" x-data="{ open: false }">
+                                                <button @click="open = !open" class="md:px-4 md:py-2 text-gray-800 hover:text-gray-900 flex items-center">
+                                                    Menu
+                                                    <svg class="w-4 h-4 ml-2 -mr-1" xmlns="http://www.w3.org/2000/svg" viewBox="0 0 20 20" fill="currentColor">
+                                                        <path fill-rule="evenodd" d="M10 12l-5-5h10l-5 5z" />
+                                                    </svg>
+                                                </button>
+                                                <div x-show="open" @click.away="open = false" class="absolute left-0 w-40 mt-1 bg-white rounded-md shadow-lg">
+                                                    <a href="#" class="block px-4 py-2 text-sm text-gray-700 hover:text-indigo-400">Option 1</a>
+                                                    <a href="#" class="block px-4 py-2 text-sm text-gray-700 hover:text-indigo-400">Option 2</a>
+                                                    <a href="#" class="block px-4 py-2 text-sm text-gray-700 hover:text-indigo-400">Option 3</a>
+                                                </div>
+                                            </li>
+                                        </ul>
+                                    </div>
+                                </div>
+                            </nav>
+                        </div>
                     </div>
                 </div>
-            </div>
+            </nav>
 
 
             <section id="home" class="bg-cover bg-center h-screen flex items-center justify-center pt-8 opacity-90" style="background-image: url('/image/kalibenda.jpg'); height: 275px">
-                <div class="text-center px-4 ">
-                    <h2 class="text-4xl md:text-7xl font-bold text-white pt-3">Profil Desa Kalibenda</h2>
-                    <p class="mt-2 text-lg font-medium text-white">Sekilas tentang sejarah, visi, dan misi Desa Kalibenda.</p>
+                <div class="text-center">
+                    <h2 class="text-4xl md:text-6xl font-extrabold text-gray-900">Profil Desa Kalibenda</h2>
+                    <p class="mt-2 text-lg font-medium text-white"><span class="bg-gray-800 rounded-md px-2 bg-bottom">Sekilas tentang sejarah, visi, dan misi Desa Kalibenda.</span></p>
                 </div>
             </section>
 
             <!-- About Section -->
-            <section id="about" class="py-20 pt-14 bg-gray-100" >
-                <div class="max-w-8xl mx-auto px-4 sm:px-6 lg:px-8">
-                    <div class="grid grid-cols-1 md:grid-cols-2 gap-8">
-                        <img src="/image/kalibenda.jpg" alt="Profil Desa" class="w-full h-auto rounded shadow-lg">
-                        <div class="flex flex-col justify-center">
-                            <h2 class=" md:text-xl text-gray-700 mb-7 font-black text-3xl text-primary uppercase">Sejarah desa Kalibenda</h2>
-                            <p class=" text-gray-700 leading-relaxed font-medium"><span class="ml-10"></span> Desa Kalibenda terletak di Kecamatan Ajibarang, Kabupaten Banyumas, Jawa Tengah, dan memiliki sejarah yang panjang serta budaya yang kental. Desa ini merupakan salah satu desa tua yang telah ada sejak masa kolonial Belanda, dan kini terus berkembang sebagai desa yang mempertahankan nilai-nilai tradisional.</p>
-                            <p class="mt-4 text-gray-700 leading-relaxed font-medium"><span class="ml-10"></span>Pada masa penjajahan Belanda, Desa Kalibenda menjadi salah satu pusat perlawanan rakyat Banyumas. Penduduk desa yang mayoritas petani ikut berjuang melawan penjajah, menjadikan desa ini penting dalam sejarah perjuangan rakyat di wilayah Banyumas. Semangat kebersamaan dan gotong royong yang diwariskan dari masa itu masih menjadi ciri khas kehidupan sosial masyarakat Kalibenda hingga kini.</p>
-                            <p class="mt-4 text-gray-700 leading-relaxed font-medium indent-8">Menurut cerita masyarakat setempat, nama "Kalibenda" berasal dari dua kata, yaitu "Kali" yang berarti sungai, dan "Benda" yang merujuk pada sebuah pohon besar yang dulunya tumbuh di pinggir sungai di desa ini. Pohon Benda tersebut menjadi tempat yang disakralkan oleh warga desa dan sering dijadikan tempat bermeditasi. Keberadaan sungai dan pohon besar ini menjadi ikon penting dalam pembentukan identitas Desa Kalibenda.</p>
-                        </div>
+            <section id="about" class="py-20 pt-14 bg-gray-100">
+                <div class="max-w-5xl mx-auto px-4 sm:px-6 lg:px-8">
+                    <!-- Judul Sejarah Desa Kalibenda -->
+                    <p class="text-center md:text-4xl text-2xl text-gray-800 mb-2  font-black text-primary uppercase opacity-0 animate-slideInUp animate-duration-1000 animate-delay-200">
+                        Sejarah Desa Kalibenda
+                    </p>
+                    
+                    <div class="grid grid-cols-1 md:grid-cols-2 gap-8 items-center">
+                        <!-- Gambar -->
+                        <img src="/image/kalibenda.jpg" alt="Profil Desa" class="  w-full  rounded shadow-lg opacity-0 animate-fadeIn animate-duration-1000">
                         
+                        <!-- Konten Text -->
+                        <div class="flex flex-col justify-center">
+                            <p class="text-gray-900 leading-relaxed font-medium opacity-0 animate-fadeIn animate-duration-1000 animate-delay-400">
+                                <span class="ml-10"></span>Desa Kalibenda terletak di Kecamatan Ajibarang, Kabupaten Banyumas, Jawa Tengah, dan memiliki sejarah yang panjang serta budaya yang kental. Desa ini merupakan salah satu desa tua yang telah ada sejak masa kolonial Belanda, dan kini terus berkembang sebagai desa yang mempertahankan nilai-nilai tradisional.
+                            </p>
+                            <p class="mt-4 text-gray-900 leading-relaxed font-medium opacity-0 animate-fadeIn animate-duration-1000 animate-delay-600">
+                                <span class="ml-10"></span>Pada masa penjajahan Belanda, Desa Kalibenda menjadi salah satu pusat perlawanan rakyat Banyumas. Penduduk desa yang mayoritas petani ikut berjuang melawan penjajah, menjadikan desa ini penting dalam sejarah perjuangan rakyat di wilayah Banyumas. Semangat kebersamaan dan gotong royong yang diwariskan dari masa itu masih menjadi ciri khas kehidupan sosial masyarakat Kalibenda hingga kini.
+                            </p>
+                            <p class="mt-4 text-gray-900 leading-relaxed font-medium indent-8 opacity-0 animate-fadeIn animate-duration-1000 animate-delay-800">
+                                Menurut cerita masyarakat setempat, nama "Kalibenda" berasal dari dua kata, yaitu "Kali" yang berarti sungai, dan "Benda" yang merujuk pada sebuah pohon besar yang dulunya tumbuh di pinggir sungai di desa ini. Pohon Benda tersebut menjadi tempat yang disakralkan oleh warga desa dan sering dijadikan tempat bermeditasi. Keberadaan sungai dan pohon besar ini menjadi ikon penting dalam pembentukan identitas Desa Kalibenda.
+                            </p>
+                        </div>
                     </div>
                 </div>
             </section>
+            
+            
+            
 
             <section class="px-80 py-10" id="visi-misi">
                 <h1 class="font-black text-3xl text-primary uppercase">Visi Dan Misi</h1>
@@ -112,8 +153,7 @@
                     <p>&copy; 2024 Desa Kalibenda.</p>
                 </div>
             </footer>
-
-</nav>     
+   
 </div>
 
 <!-- <script>
@@ -131,6 +171,11 @@
 </script> -->
 
 </body>
+<script>
+    * {
+        font-family: "Poppins", serif;
+    }
+</script>
 </html>
 
 
